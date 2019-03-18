d8457ada232c274cd78d829755c47d077c34f8e4
Anders Vrethem <dersi_91@hotmil.com>
2019-03-18 13:51:07 +0100

    Squash commits to remove sensitive data
    Cleanup && Updates rebased interactively to remove sensitive information
    
    Add .firebaserc
    
    Try implementing FirebasePerformanceMonitoring
    
    Prepare iOS emulation
    
    Remove config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'vision', loadChildren: './vision/vision.module#VisionPageModule' },
 ];
 
 @NgModule({
