9e0b71f6c85fb17c8eff55f729f168b0be071961
d0rshBooken <andvr916@student.liu.se>
2019-03-24 15:08:07 +0100

    Removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyB9qcFQ_K4pBCTvaEa4E1_MXpHBQxTcQ0Q',
-    authDomain: 'hotdogorlegs-7a2ee.firebaseapp.com',
-    databaseURL: 'https://hotdogorlegs-7a2ee.firebaseio.com',
-    projectId: 'hotdogorlegs-7a2ee',
-    storageBucket: 'hotdogorlegsbucket',
-    messagingSenderId: '405806549953'
-  }
 };
 
 /*
