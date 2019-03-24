3c2108e21436c6738ff9c1cc00ddc0fcc06ab924
Anders Vrethem <dersi_91@hotmil.com>
2019-03-24 14:25:36 +0100

    Prepare iOS emulation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { FirestorageServiceService } from '../services/firestorage-service.service';
-import { ToastController, LoadingController } from '@ionic/angular';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
 import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
@@ -15,6 +15,7 @@ export class HomePage {
   image;
   collection;
   progress = false;
+  firebasePlugin = null;
 
 
   constructor(
@@ -24,9 +25,41 @@ export class HomePage {
     private toastCtrl: ToastController,
     private loadingCtrl: LoadingController,
     private afs: AngularFirestore,
-  ) { }
+    private platform: Platform
+  ) {
+    this.platform.ready().then(() => {
+      this.firebasePlugin = window['FirebasePlugin'];
+    })
+  }
+
+  ionViewDidEnter() {
+    if (this.firebasePlugin == null) {
+      console.error('FirebasePlugin not initialized before ionViewDidEnter()')
+      this.firebasePlugin.logEvent('select_content',
+        {
+          content_type: 'page_view',
+          item_id: 'FirebasePlugin not initialized before ionViewDidEnter()'
+        });
+
+    } else {
+      this.firebasePlugin.stopTrace('AppInitialized_to_ionViewDidEnter()')
+      // tslint:disable-next-line:no-console
+      console.timeEnd('AppInitialized_to_ionViewDidEnter()')
+    }
+  }
+
 
   openImagePicker() {
+
+    // tslint:disable-next-line:no-console
+    console.time('trace "select-image"')
+    this.firebasePlugin.startTrace('select-image',
+      (success: any) => {
+        // tslint:disable-next-line:no-console
+      },
+      (err: any) => console.error('Could not create trace "select-image"'))
+
+
     this.imagePicker.hasReadPermission().then(
       (result) => {
         if (result === false) {
@@ -38,6 +71,9 @@ export class HomePage {
           }).then(
             (results) => {
               for (let i = 0; i < results.length; i++) {
+                this.firebasePlugin.stopTrace('select-image')
+                // tslint:disable-next-line:no-console
+                console.timeEnd('trace "select-image"')
                 this.uploadImageToFirebase(results[i]);
               }
             }, (err) => console.log(err)
@@ -64,6 +100,14 @@ export class HomePage {
       token += letters[Math.floor(Math.random() * letters.length)]
     }
 
+    // tslint:disable-next-line:no-console
+    console.time('trace "upload-image"')
+    this.firebasePlugin.startTrace('upload-image',
+      (success: any) => {
+
+      },
+      (err: any) => console.error('Could not create trace "test_trace"'))
+
     // uploads img to firebase storage
     this.fsService.uploadImage(image, token)
       .then(async url => {
@@ -80,6 +124,9 @@ export class HomePage {
           duration: 3000
         });
         toast.present();
+        this.firebasePlugin.stopTrace('upload-image')
+        // tslint:disable-next-line:no-console
+        console.timeEnd('trace "upload-image"')
       }, async (err) => {
         const toast = await this.toastCtrl.create({
           message: err,
@@ -88,7 +135,5 @@ export class HomePage {
         toast.present();
         console.error(err);
       });
-
-
   }
 }
