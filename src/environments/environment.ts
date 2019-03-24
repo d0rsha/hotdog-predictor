f8b74fabc698ac6eab2f4c298d95482e848470cf
d0rshBooken <andvr916@student.liu.se>
2019-03-24 15:09:34 +0100

    Altered

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { _environment_with_firebase_config } from './_environment';
 
-export const environment = {
-  production: false,
-};
+
+export const environment = _environment_with_firebase_config
 
 /*
  * For easier debugging in development mode, you can import the following file
