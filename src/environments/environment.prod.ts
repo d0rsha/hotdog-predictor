dbc79e5be5b3a1cc2bfcfa1d2e4802a997f42594
d0rshBooken <andvr916@student.liu.se>
2019-03-24 15:47:20 +0100

    Added firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+import { _firebase_config } from './_firebase_config';
+
+
+export const environment = {
+  production: true,
+  firebase: _firebase_config,
+}
